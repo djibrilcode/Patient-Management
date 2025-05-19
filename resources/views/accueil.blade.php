a29803bb9e43e24ad544adadd975f2662b12f2c1
souley <souley@gmail>
2025-05-19 23:20:16 +0100

    Description claire des modifications effectuées

--- resources/views/accueil.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <h1>Bienvenue sur la page d'accueil du cabinet médical</h1>
-        <p>Bienvenue dans votre gestion de cabinet médical. Vous pouvez gérer les consultations, les rendez-vous, et plus encore.</p>
-    </div>
-@endsection
